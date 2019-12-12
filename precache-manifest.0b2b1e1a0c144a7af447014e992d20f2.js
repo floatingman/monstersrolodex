f9fa1e1b083d3f97660d548f01e06203192ccef9
Daniel Newman <dwnewman78@gmail.com>
2019-12-11 22:03:43 -0600

    Updates

---
+++ precache-manifest.0b2b1e1a0c144a7af447014e992d20f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7667c3c236b8f0df618fbacad5ae3fe0",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "20cf0c22c6ecc0a20d68",
+    "url": "/monstersrolodex/static/css/main.6bbe9cea.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monstersrolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monstersrolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "20cf0c22c6ecc0a20d68",
+    "url": "/monstersrolodex/static/js/main.2dd20a07.chunk.js"
+  },
+  {
+    "revision": "ca2709bd436d545a2d66",
+    "url": "/monstersrolodex/static/js/runtime-main.257d90d8.js"
+  }
+]);
